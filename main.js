27700b52d6880f47051e1e3076102185af3421f8
Szeplaki Dominik <szeplaki.dominik@BOLYAI.LOCAL>
2024-09-27 12:58:08 +0200

    Js cuccos

---
+++ main.js
@@ -0,0 +1,68 @@
+const array = [
+    {
+        firstname1: 'Géza',
+        firstname2: 'Ferenc',
+        lastname: 'Kocsis'
+    },
+    {
+        firstname1: 'Mária',
+        firstname2: 'Júlia',
+        lastname: 'Horváth'
+    },
+    {
+        firstname1: 'Ferenc',
+        lastname: 'Balogh'
+    },
+    {
+        firstname1: 'Gábor',
+        firstname2: 'Attila',
+        lastname: 'Horváth'
+    },
+]
+
+const table = document.createElement('table')
+document.body.appendChild(table)
+
+const tablehead = document.createElement('thead')
+table.appendChild(tablehead)
+
+const tableheadrow = document.createElement('tr')
+tablehead.appendChild(tableheadrow)
+
+const th = document.createElement('th')
+tableheadrow.appendChild(th)
+
+const th2 = document.createElement('th')
+tableheadrow.appendChild(th2)
+
+th.innerHTML="Veznev"
+th2.innerHTML="Kernev"
+th2.colSpan = 2
+
+const tablebody = document.createElement('tbody')
+table.appendChild(tablebody)
+for(const person of array){
+    const tr = document.createElement('tr')
+    const lastname = document.createElement('td')
+    tablebody.appendChild(tr)
+    tr.appendChild(lastname)
+    lastname.innerHTML = person.lastname
+    const firstname = document.createElement('td')
+    tablebody.appendChild(tr)
+    tr.appendChild(firstname)
+    firstname.innerHTML = person.firstname1
+    
+    
+    tablebody.appendChild(tr)
+    
+
+    if(person.firstname2===undefined){
+        firstname.colSpan = 2
+    }
+    else{
+        const firstname2 = document.createElement('td')
+        firstname2.innerHTML = person.firstname2
+        tr.appendChild(firstname2)
+
+    }
+}
